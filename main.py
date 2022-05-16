a753189adfdbbf8cb85805bd836f659a7cee883f
Shreyas Banagar <shreyas.banagar@zycus.com>
2022-05-16 13:52:36 +0530

    First working version of scraping

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-from selenium import webdriver
 import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 def testing_web_scrapper_code():
     searchString = "tablets"
@@ -25,15 +29,39 @@ def testing_web_scrapper_code_ineuron():
         f.write(ineuron_html.get_text())
 
 def testing_selenium_code():
-    with webdriver.Chrome(executable_path=driver_path) as driver:
-        driver.get("https://courses.ineuron.ai/")
-        def scroll_to_end(wd):
-            wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://courses.ineuron.ai/")
+    
+    def infinite_scroll(driver):
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(1)
-        
-        scroll_to_end(driver)
-        print(driver.title)
-driver_path = "chromedriver.exe"
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            
+            if new_height == last_height:
+                break
+            last_height = new_height
+    infinite_scroll(driver)
+    ineuronPage = driver.page_source
+    ineuron_html = bs(ineuronPage, "html.parser")
+    titles_found = ineuron_html.findAll(class_="Course_course-title__2rA2S")
+    prices_found = ineuron_html.findAll(class_="Course_course-price__3-3_U")
+    desc_found = ineuron_html.findAll(class_="Course_course-desc__2G4h9")
+    count=0
+    titles = []
+    prices = []
+    descriptions = []
+    for title,price,description in zip(titles_found,prices_found,desc_found):
+        count+=1
+        titles.append(title.text)
+        prices.append(price.text)
+        descriptions.append(description.text)
+    print(titles)
+    print(prices)
+    print(len(prices_found))
+    print(len(titles))
+    print(len(descriptions))
 #testing_web_scrapper_code()
 #testing_web_scrapper_code_ineuron()
 testing_selenium_code()
