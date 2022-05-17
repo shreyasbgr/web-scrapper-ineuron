7cc70b1c1009f336215a6163fc3b5649db032ea2
Shreyas Banagar <shreyas.banagar@zycus.com>
2022-05-17 17:30:05 +0530

    Added push to mongodb step (working)

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-import requests
+# Import beautifoul soup
 from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen as uReq
+
+from mongo_util import mongodb_util
 import time
 
+# Imports for selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-
-def testing_web_scrapper_code():
-    searchString = "tablets"
-    flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-    uClient = uReq(flipkart_url)
-    flipkartPage = uClient.read()
-    uClient.close()
-    flipkart_html = bs(flipkartPage, "html.parser")
-    bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-    print(flipkart_html)
-
-def testing_web_scrapper_code_ineuron():
-    ineuron_url = "https://courses.ineuron.ai/"
-    uClient = uReq(ineuron_url)
-    ineuronPage = uClient.read()
-    uClient.close()
-    ineuron_html = bs(ineuronPage, "html.parser")
-    bigboxes = ineuron_html.findAll("div", {"class": "Course_course-card__1_V8S Course_card__2uWBu card"})
-    with open('testfile','w') as f:
-        f.write(ineuron_html.get_text())
-
 
 def testing_selenium_code():
     driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -57,6 +37,13 @@ def testing_selenium_code():
         description = course.find(class_="Course_course-desc__2G4h9")
         instructor = course.find(class_="Course_course-instructor__1bsVq")
         price = course.find(class_="Course_course-price__3-3_U")
+        record = {
+            "title": title.text if title else '',
+            "description": description.text if description else '',
+            "instructor": instructor.text if instructor else '',
+            "price": price.text if price else ''
+        }
+        my_mongo_util_obj.insert_one_record(record)
         titles.append(title.text if title else '')
         descriptions.append(description.text if description else '')
         instructors.append(instructor.text if instructor else '')
@@ -69,6 +56,13 @@ def testing_selenium_code():
     print(len(descriptions))
     print(len(instructors))
     print(len(prices))
-#testing_web_scrapper_code()
-#testing_web_scrapper_code_ineuron()
-testing_selenium_code()
+
+if __name__ == '__main__':
+    global my_mongo_util_obj
+
+    connection_url = "mongodb+srv://root:root@myatlascluster.5nfni.mongodb.net/reviews_scrapper_db?retryWrites=true&w=majority"
+    db_name = 'reviews_scrapper_db'
+    col_name = 'crawlerDb'
+    logfile = 'output.log'
+    my_mongo_util_obj = mongodb_util(connection_url,db_name,col_name,logfile)
+    testing_selenium_code()
